14e301d54a1851c1e2ea49bc4f5f99a0d3bba7d4
Lautaro Martinez <lautipro2006@gmail.com>
2025-08-27 21:31:22 -0300

    JS Avatar. Se pasan a variables globales las variables correspondientes

--- Practicas/Avatar 27-8-25/script.js
+++ Practicas/Avatar 27-8-25/script.js
@@ -1,6 +1,3 @@
-var selectedCharacter = null;
-var rulesVisible = false;
-
 const attacks = ['Puño', 'Patada', 'Barrida'];
 const characters = ['Zuko', 'Katara', 'Aang', 'Toph'];
 const beats = {'Puño': 'Barrida', 'Patada': 'Puño', 'Barrida': 'Patada'};
@@ -11,57 +8,75 @@ const imgByCharacter = {
     'Toph': 'https://i.pinimg.com/736x/4e/00/11/4e00111c746f99e1a41374cf8ebb77c7.jpg'
 }
 
+const elAttacksContainer = document.getElementById('attacks-container');
+const elShowRulesButton = document.getElementById('show-rules');
+const elRulesContainer = document.getElementById('rules-container');
+const elAttackSection = document.getElementById('attack-section');
+const elCharSelector = document.getElementById('char-selector');
+const elPlayerImg = document.getElementById('player');
+const elEnemyImg = document.getElementById('enemy');
+const elVersusText = document.getElementById('versus-text');
+const elBattleResult = document.getElementById('battle-result');
+const elFinish = document.getElementById('finish');
+const elFinishText = document.getElementById('finish-text');
+const elCharacterRadios = document.getElementsByName('character');
+const elPlayerHP = document.getElementById('player-hp');
+const elEnemyHP = document.getElementById('enemy-hp');
+const elPlayerFloatingText = document.getElementById('player-floating-text');
+const elEnemyFloatingText = document.getElementById('enemy-floating-text');
+const elAttackTitle = document.getElementById('attack-title');
+
+var selectedCharacter = null;
+var rulesVisible = false;
+var healthPointsByCharacter = { 'player': 3, 'enemy': 3 };
+
 attacks.forEach(attack => {
     var button = document.createElement('button');
     button.id = 'boton-' + attack;
     button.textContent = attack;
     button.onclick = () => attackFunction(attack);
 
-    document.getElementById('attacks-container').appendChild(button);
+    elAttacksContainer.appendChild(button);
 });
 
 // Funciones de Botones
-
 function showRules() {
     rulesVisible = !rulesVisible;
 
     if(rulesVisible) {
-        document.getElementById('show-rules').innerText = "Ocultar Reglas";
-        toggleDisplay('rules-container', true);
+        elShowRulesButton.innerText = "Ocultar Reglas";
+        toggleDisplay(elRulesContainer, true);
     } else {
-        document.getElementById('show-rules').innerText = "Mostrar Reglas";
-        toggleDisplay('rules-container', false);
+        elShowRulesButton.innerText = "Mostrar Reglas";
+        toggleDisplay(elRulesContainer, false);
     }
 }
 
 function selectCharacter() {
-    const radios = document.getElementsByName('character');
     var hasOption = false;
-    for (const radio of radios) {
+    for (const radio of elCharacterRadios) {
         if (radio.checked) {
             const enemyCharacter = randomItem(characters);
             hasOption = true;
             selectedCharacter = radio.value;
-            toggleDisplay('attack-section', true);
-            document.getElementById('player').src = imgByCharacter[selectedCharacter];
-            document.getElementById('enemy').src = imgByCharacter[enemyCharacter];
-            document.getElementById('versus-text').innerText = `Seleccionaste a ${selectedCharacter} y tu enemigo a ${enemyCharacter}`;
+            toggleDisplay(elAttackSection, true);
+            elPlayerImg.src = imgByCharacter[selectedCharacter];
+            elEnemyImg.src = imgByCharacter[enemyCharacter];
+            elVersusText.innerText = `Seleccionaste a ${selectedCharacter} y tu enemigo a ${enemyCharacter}`;
         }
     }
     
     if(!hasOption) {
         alert(`Seleccione un Personaje`);
     } else {
-        toggleDisplay('char-selector', false);
+        toggleDisplay(elCharSelector, false);
     }
 }
 
 function attackFunction(playerAttack) {
     const enemyAttack = randomItem(attacks);
 
-    if (
-        (beats[playerAttack] === enemyAttack)
-    ) {
+    if (beats[playerAttack] === enemyAttack) {
         newAction('¡Le Pegaste!', 'limegreen');
         lowerLife('enemy');
     } else if (playerAttack === enemyAttack) {
@@ -75,21 +90,23 @@ function attackFunction(playerAttack) {
     newAction(resultMessage);
 } 
 
-// Funciones internas de pelea
 
+// Funciones internas de pelea
 function lowerLife(target) {
     damageEffect(target);
-    var lifePointsContainer = document.getElementById(target + '-lp');
-    const lp = parseInt(lifePointsContainer.innerText) - 1;
-    lifePointsContainer.innerText = lp;
+    var healthPointsContainer = (target === 'player') ? elPlayerHP : elEnemyHP;
+    var hp = healthPointsByCharacter[target] - 1;
+
+    healthPointsContainer.innerText = hp;
+    healthPointsByCharacter[target] = hp;
     
-    if(lp == 0) { 
+    if(hp == 0) { 
         target == 'enemy' ? showFinish(`Ganaste!`) : showFinish(`Perdiste...`);
-        toggleDisplay('attacks-container', false);
-        toggleDisplay('attack-title', false);
+        toggleDisplay(elAttacksContainer, false);
+        toggleDisplay(elAttackTitle, false);
         setTimeout(function() {
-            document.getElementById('finish').style.display = 'flex';
-            document.getElementById('finish').classList.add('fade-in')
+            elFinish.style.display = 'flex';
+            elFinish.classList.add('fade-in')
         }, 1200);
         deadEffect(target);
     }
@@ -100,25 +117,19 @@ function newAction(resultMessage, color) {
     action.innerHTML = resultMessage;
     action.style.color = color || 'inherit';
 
-    var resultContainer = document.getElementById('battle-result');
-    resultContainer.insertBefore(action, resultContainer.firstChild);
+    elBattleResult.insertBefore(action, elBattleResult.firstChild);
 }
+
 
 // Efectos visuales
-
 function damageEffect(target) {
-  const img = document.getElementById(target);
-  img.classList.add('effect');
-  setTimeout(() => img.classList.remove('effect'), 300);
-  
-  const msg = document.getElementById(target+'-floating-text');
-  msg.classList.add('float-up');
-  setTimeout(() => msg.classList.remove('float-up'), 1000);
-}
-
-function damageEffect(target) {
-    addEffect(target, 'effect', 300);
-    addEffect(target+'-floating-text', 'float-up', 1000);
+    if (target === 'player') {
+        addEffect('player', 'effect', 300);
+        addEffect('player-floating-text', 'float-up', 1000);
+    } else {
+        addEffect('enemy', 'effect', 300);
+        addEffect('enemy-floating-text', 'float-up', 1000);
+    }
 }
 
 function deadEffect(target) {
@@ -126,22 +137,21 @@ function deadEffect(target) {
 }
 
 function showFinish(text){
-    const finish = document.getElementById('finish-text');
-    finish.innerHTML = text;
+    elFinishText.innerHTML = text;
 }
 
-// Funciones Repetidas
 
-function toggleDisplay(id, show) {
-    document.getElementById(id).style.display = show ? 'block' : 'none';
+// Funciones Repetidas
+function toggleDisplay(el, show) {
+    el.style.display = show ? 'block' : 'none';
 }
 
 function randomItem(list) {
     return list[Math.floor(Math.random() * list.length)];
 }
 
-function addEffect(target, className, duration) {
-    const el = document.getElementById(target);
+function addEffect(id, className, duration) {
+    const el = document.getElementById(id);
     el.classList.add(className);
     if (duration > 0) {
         setTimeout(() => el.classList.remove(className), duration);
