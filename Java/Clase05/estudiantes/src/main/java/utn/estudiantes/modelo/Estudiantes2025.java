af2a726dd812b4d737c1238d51844a2baa5a70a5
Francisco Elías <elias.fran.fe@gmail.com>
2025-09-15 22:56:15 -0300

    Clase 5 Java

---
+++ Java/Clase05/estudiantes/src/main/java/utn/estudiantes/modelo/Estudiantes2025.java
@@ -0,0 +1,26 @@
+package utn.estudiantes.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+// boilerplate - Codigo Repetitivo
+@Data // Aqui se crean los getters and setters
+@NoArgsConstructor // Aqui se crea el constructor vacio
+@AllArgsConstructor // Aqui se crea el constructor con todos lo atributos
+@ToString // Aquí se crea el método ToString
+public class Estudiantes2025 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idestudiantes2025;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String email;
+}
