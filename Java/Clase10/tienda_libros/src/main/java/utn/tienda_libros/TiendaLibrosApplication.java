b552fff2c5ffdeee91fb626d37223a3ba3a072f0
Dilancio <diramos14@gmail.com>
2025-10-22 21:11:58 -0300

    Clase 10 Java - funciona

--- Java/Clase10/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
+++ Java/Clase10/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
@@ -11,18 +11,18 @@ import java.awt.EventQueue;
 @SpringBootApplication
 public class TiendaLibrosApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		ConfigurableApplicationContext contextoSpring =
-				new SpringApplicationBuilder(TiendaLibrosApplication.class)
-						.headless(false)
-						.web(WebApplicationType.NONE)
-						.run(args);
-		//Ejecutamos el codigo para cargar el formulario
-		EventQueue.invokeLater(() -> { //metodo lambda
-			//Obtenemos el objeto from a traves del spring
-			LibroFrom libroFrom = contextoSpring.getBean(LibroFrom.class);
-			libroFrom.setVisible(true);
-		});
-	}
+        ConfigurableApplicationContext contextoSpring =
+                new SpringApplicationBuilder(TiendaLibrosApplication.class)
+                        .headless(false)
+                        .web(WebApplicationType.NONE)
+                        .run(args);
+        //Ejecutamos el codigo para cargar el formulario
+        EventQueue.invokeLater(() -> { //metodo lambda
+            //Obtenemos el objeto from a traves del spring
+            LibroFrom libroFrom = contextoSpring.getBean(LibroFrom.class);
+            libroFrom.setVisible(true);
+        });
+    }
 }
