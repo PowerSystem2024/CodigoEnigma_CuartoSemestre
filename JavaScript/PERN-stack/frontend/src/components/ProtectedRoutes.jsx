082ef0c64b0db723550453219077f42ba572f442
Eliana <crisely.cye@gmail.com>
2025-10-31 17:13:25 -0300

    borrado temporal

--- JavaScript/PERN-stack/frontend/src/components/ProtectedRoutes.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-
-export const ProtectedRoutes = ({ redirectTo, children, isAllowed }) => {
-    if (!isAllowed) {
-        return <Navigate to={redirectTo} replace />;
-    }
-
-    return children ? children : <Outlet />;
-};
--- JavaScript/PERN-stack/frontend/src/components/ui/index.js
+++
@@ -1,5 +0,0 @@
-export { Button } from './Button.jsx';
-export { Card } from './Card.jsx';
-export { Input } from './Input.jsx';
-export { Label } from './Label.jsx';
-export { Container } from './Container.jsx';
